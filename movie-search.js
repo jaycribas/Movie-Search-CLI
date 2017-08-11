448f628dc11dbaeabd1d99f2d29edfdd174c5a1d
Jay Cribas <feverjay@gmail.com>
2017-08-11 14:25:49 -0700

    GET request pulls HTML code

--- movie-search.js
+++ movie-search.js
@@ -1,2 +1,21 @@
 const cheerio = require('cheerio')
 const http = require('http')
+
+const query = process.argv[2]
+
+searchImdb(query)
+
+function searchImdb(searchTerm){
+  http.get(
+    `http://www.imdb.com/find?ref_=nv_sr_fn&q=${searchTerm}&s=all`,
+    response => {
+      let body = ''
+
+      response.on('data', data => body += data)
+
+      response.on('end', () => {
+        console.log("body (╯°□°）╯︵ ┻━┻", body)
+      })
+    }
+  )
+}
